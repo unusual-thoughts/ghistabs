0551e9cbae2203448c8de1dabaacf96a50c1bcd0
Henri Chain <henri@henri-chain.com>
2026-08-01 03:48:17 +0200

    remove old files

--- ghidra_scripts/sample_script.py
+++
@@ -1,8 +0,0 @@
-# Sample PyGhidra GhidraScript
-# @category Examples
-# @runtime PyGhidra
-
-from java.util import LinkedList
-java_list = LinkedList([1,2,3])
-
-block = currentProgram.memory.getBlock('.text')
